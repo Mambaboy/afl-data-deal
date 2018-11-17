7d51371a8a0650507e1f85663c921bbcd440c4e4
xiaosatianyu <xiaosatianyu@126.com>
2018-11-17 21:35:02 +0800

    learn: c call python

---
+++ c-python-learn/build.sh
@@ -0,0 +1,1 @@
+g++ test.cpp -I /usr/include/python2.7 -L /usr/lib/python2.7/config-x86_64-linux-gpu -lpython2.7
---
+++ c-python-learn/xiaosa.py
@@ -0,0 +1,7 @@
+import  sys
+import os
+import time
+def pp(a, b):
+    print 'add ', a, ' and ', b
+    print time.time() 
+    return a+b
